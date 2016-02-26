bab2f6edb69ff3adb6f5bc4754b37be3d0ab7989
Muhammad Jalloh <dev@blogmatiq.com>
2016-02-26 17:13:42 -0500

    Updated projects settinsg config

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect 
 
 # Create your views here.
+def home(request):
+	return HttpResponse("Home of Blogmatiq :)")
+
+def blogs(request):
+	return HttpResponse("List of Blogs for user")
+
+
